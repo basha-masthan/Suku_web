ecbdbc2010ffd400159ca52f21169312aa458572
basha-masthan <bashamasthan31@gmail.com>
2024-08-20 04:38:37 +0530

    vercel

--- final1/settings.py
+++ final1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p4d_)mtv^#0yeyijzlda29gnv^8xm##sqn1fy02+2)w#bkq=4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','76.76.21.142']
 
 
 # Application definition
